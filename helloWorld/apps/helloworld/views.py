88f037f34e769caa85a3e7d4029657af0f9c37d8
Juanjo Llamas <juanjo.llamas.caeiro@gmail.com>
2014-01-16 19:35:44 +0100

    Update views.py

--- helloWorld/apps/helloworld/views.py
+++ helloWorld/apps/helloworld/views.py
@@ -3,6 +3,6 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 
 def index_view(request):
-	message = "hello world django"
+	message = "hello world django. You have a Linux Django Application running on Windows."
 	ctx = {'message': message}
 	return render_to_response('helloworld/index.html', ctx, context_instance=RequestContext(request))
